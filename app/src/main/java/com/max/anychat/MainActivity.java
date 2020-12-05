be2d2747feaf17014d6270b87c8c90ef9e5c761a
smaxiso <44551068+smaxiso@users.noreply.github.com>
2020-12-05 12:49:03 +0530

    Added optimization in number selection when pasted with country code

--- app/src/main/java/com/max/anychat/MainActivity.java
+++ app/src/main/java/com/max/anychat/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -29,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
         Button contact = (Button)findViewById(R.id.contact);
         final EditText phone = (EditText)findViewById(R.id.phone);
         final EditText cc = (EditText)findViewById(R.id.cc);
-        final TextView text = (TextView)findViewById(R.id.about);
+        cc.setEnabled(false);   //currently can't edit country code, India is default.
+        final TextView numtext = (TextView)findViewById(R.id.nutext);
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        String pasteData = "";
 
 
         //Hide keyboard when touched on the screen
@@ -42,24 +46,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Chat button click function
-        chat.setOnClickListener(new View.OnClickListener() {
+        chat.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-
-                if(cc.getText().toString().length()!=2)
+            public void onClick(View v)
+            {
+                String  temp = phone.getText().toString();
+                temp = temp.substring(temp.length()-10);
+                if(cc.getText().toString().length()==0)
                 {
                     cc.setError("Enter valid country code !!");
                 }
-                else if (phone.getText().toString().length()!=10)
+                else if (temp.length()!=10)
                 {
-                    phone.setError("Enter valid Mobile Number !!");
+                    phone.setError("Mobile no. length should be 10");
+                    numtext.setText("");
                 }
-                else {
-                    String link = "https://wa.me/" + cc.getText().toString() + phone.getText().toString();
-                    //text.setText(link);
-                    Uri uri = Uri.parse(link); // missing 'http://' will cause crashed
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
+                else
+                {
+                    boolean flag = check(temp);   //check phone no.'s correct format
+                    if (flag)
+                    {
+                        //numtext.setText("+91 - " + phone.getText().toString().substring(0,5) + " - " + phone.getText().toString().substring(5,10));
+                        numtext.setText("+91 - " + temp.substring(0,5) + " - " + temp.substring(5,10));
+                        phone.setText(temp);
+                        String link ="https://wa.me/91" + temp;
+                        Uri uri = Uri.parse(link); // missing 'http://' will cause crashed
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        //Toast.makeText(MainActivity.this, "Invalid Mobile Number !", Toast.LENGTH_SHORT).show();
+                        alert();
+                        phone.setText("");
+                        numtext.setText("");
+                    }
                 }
             }
         });
@@ -68,12 +90,24 @@ public class MainActivity extends AppCompatActivity {
         contact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse("https://smaxiso.github.io"); // missing 'http://' will cause crashed
+                Uri uri = Uri.parse("https://smaxiso.github.io/contact.html"); // missing 'http://' will cause crashed
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
         });
 
+    }
+
+    //function to check if valid number is entered or not
+    public boolean check(String s)
+    {
+        //s = s.substring(s.length() - 10);
+        if (s.charAt(0)=='9' || s.charAt(0)=='8' || s.charAt(0)=='7' || s.charAt(0)=='6')
+        {
+            return true;
+        }
+        else
+            return false;
     }
 
     //function to hide keyboard
@@ -88,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setTitle("Confirm Exit??");
         alertDialogBuilder.setMessage("Are you sure want to exit the app?");
-        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setCancelable(true);
         alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -105,4 +139,20 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alertDialog=alertDialogBuilder.create();
         alertDialog.show();
     }
+
+    //show alert message prompting to enter the correct mobile number format
+    public void alert()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Please enter valid Mobile Number !!");
+        builder.setMessage("Mobile telephone numbering in India start with 9, 8, 7 or 6 which is based on GSM, WCDMA and LTE technologies.");
+        builder.setCancelable(true);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int id) {}
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
 }
